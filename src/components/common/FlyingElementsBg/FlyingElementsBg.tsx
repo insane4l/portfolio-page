bfca87921866594cbeb9e4c796976df519d80edc
insane4l <43768644+insane4l@users.noreply.github.com>
2022-05-01 23:45:34 +0300

    Animation with flying items created

---
+++ src/components/common/FlyingElementsBg/FlyingElementsBg.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './FlyingElementsBg.scss'
+
+export const FlyingElementsBg: React.FC<FlyingElementsBgPropsType> = React.memo( ({elCount = 130}) => {
+
+    const dots = []
+    for (let i = 1; i <= elCount; i++) {
+        dots.push(i)
+    }
+
+    const mappedDots = dots.map(elNum => {
+        return (
+            <div className={`el__wrapper el__wrapper-${elNum}`}>
+                <div className={`el el-${elNum}`}></div>
+            </div>
+        )
+    })
+
+    return (
+        <div className="overlay">
+            {mappedDots}
+        </div>
+    )
+})
+
+
+type FlyingElementsBgPropsType = {
+    elCount?: number
+}
